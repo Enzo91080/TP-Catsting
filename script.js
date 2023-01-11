e1881ec3873750515c780612a156fa3588596e97
Enzo <enzo.aime91@gmail.com>
2023-01-11 10:35:47 +0100

    adding cards to templates

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
+function toggleMobileMenu(menu) {
+    menu.classList.toggle("open");
+}
+function darkTheme() {
+
+    const body = document.querySelector("body"),
+    nav = document.querySelector("nav"),
+    modeToggle = document.querySelector(".dark-light");
+
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark-mode") {
+    body.classList.add("dark");
+}
+
+// js code to toggle dark and light mode
+modeToggle.addEventListener("click", () => {
+    modeToggle.classList.toggle("active");
+    body.classList.toggle("dark");
+
+    // js code to keep user selected mode even page refresh or file reopen
+    if (!body.classList.contains("dark")) {
+        localStorage.setItem("mode", "light-mode");
+    } else {
+        localStorage.setItem("mode", "dark-mode");
+    }
+});
+}
+
 function init() {
     darkTheme();
     // Affichage accueil
     recupNbAnnecdote();
     recupNbRaces();
+    recupPelageFrequent();
     // Affichage annecdotes
 
     recupAnecdote();
@@ -44,4 +73,65 @@ function recupNbRaces() {
     });
 }
 
+async function recupPelage() {
+    return new Promise((resolve) => {
+      sendRequest("https://catfact.ninja/breeds").then(async (response) => {
+        let lastpage = response.last_page;
+        let arrayPelage = [];
+        for (let index = 1; index <= lastpage; index++) {
+          let response = await sendRequest(
+            "https://catfact.ninja/breeds?page=" + index
+          );
+  
+          response.data.forEach((race) => {
+            arrayPelage.push(race.coat);
+          });
+        }
+        // setInterval(() => {
+        // }, 2000);
+        resolve(arrayPelage);
+      });
+    });
+  }
 
+
+async function recupPelageFrequent() {
+    let typePelage = document.querySelector(".typePelage");
+    let arrayPelage = await recupPelage();
+    let nombreMax = 1;
+    let nombreActuel = 0;
+    let lePlusFrequent;
+  
+    for (let i = 0; i < arrayPelage.length; i++) {
+      for (let f = i; f < arrayPelage.length; f++) {
+        if (arrayPelage[i] == arrayPelage[f]) {
+          nombreActuel++;
+        }
+        if (nombreMax < nombreActuel) {
+          nombreMax = nombreActuel;
+          lePlusFrequent = arrayPelage[i];
+        }
+      }
+      nombreActuel = 0;
+    }
+    typePelage.innerHTML = lePlusFrequent;
+  }
+  
+
+
+
+
+
+// Récupération d'une anecdote aléatoire
+function recupAnecdote() {
+    let buttonGetAnecdote = document.querySelector(".getAnecdote");
+    let annecdote = document.querySelector(".anecdoteRandom");
+    buttonGetAnecdote.addEventListener("click", () => {
+        sendRequest("https://catfact.ninja/fact").then((response) => {
+            annecdote.textContent = response.fact;
+        });
+    });
+    
+}
+
+  
